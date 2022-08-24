1bb47e6258a5157bbca0ea9bbfc26a1e515c4bb6
yunhalee05 <yunhalee0506@naver.com>
2022-08-25 03:30:29 +0900

    Test: Layout 인수테이스 예외 케이스 추가

--- src/test/java/com/yunhalee/flo/layout/LayoutAcceptanceTest.java
+++ src/test/java/com/yunhalee/flo/layout/LayoutAcceptanceTest.java
@@ -21,6 +21,8 @@ public class LayoutAcceptanceTest extends AcceptanceTest {
     private static final int FIRST_PRODUCT_PRICE = 30;
     private static final String SECOND_PRODUCT_NAME = "secondProduct";
     private static final int SECOND_PRODUCT_PRICE = 20;
+    private static final String THIRD_PRODUCT_NAME = "thirdProduct";
+    private static final int THIRD_PRODUCT_PRICE = 10;
 
     @Test
     void create_layout() {
@@ -32,6 +34,20 @@ public class LayoutAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> createLayoutResponse = create_layout_request(NAME, Arrays.asList(firstProductId, secondProductId));
         // then
         check_layout_created(createLayoutResponse);
+    }
+
+    @Test
+    void create_layout_with_already_existing_name_is_invalid() {
+        // given
+        String firstProductId = create_product(FIRST_PRODUCT_NAME, FIRST_PRODUCT_PRICE);
+        ExtractableResponse<Response> createFirstLayoutResponse = create_layout_request(NAME, Arrays.asList(firstProductId));
+        check_layout_created(createFirstLayoutResponse);
+        String secondProductId = create_product(SECOND_PRODUCT_NAME, SECOND_PRODUCT_PRICE);
+
+        // when
+        ExtractableResponse<Response> createLayoutResponse = create_layout_request(NAME, Arrays.asList(firstProductId, secondProductId));
+        // then
+        check_layout_not_created(createLayoutResponse);
     }
 
     @Test
@@ -47,6 +63,18 @@ public class LayoutAcceptanceTest extends AcceptanceTest {
         // then
         check_layout_found(findLayoutResponse);
     }
+
+    @Test
+    void find_layout_with_not_existing_id_is_invalid() {
+        // given
+        String id = "invalidId";
+
+        // when
+        ExtractableResponse<Response> findLayoutResponse = find_layout_request(id);
+        // then
+        check_layout_not_found(findLayoutResponse);
+    }
+
 
     @Test
     void find_layouts() {
@@ -80,6 +108,25 @@ public class LayoutAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
+    void update_layout_with_already_existing_name_is_invalid() {
+        // given
+        String firstProductId = create_product(FIRST_PRODUCT_NAME, FIRST_PRODUCT_PRICE);
+        ExtractableResponse<Response> createFirstLayoutResponse = create_layout_request(NAME, Arrays.asList(firstProductId));
+        check_layout_created(createFirstLayoutResponse);
+        String secondProductId = create_product(SECOND_PRODUCT_NAME, SECOND_PRODUCT_PRICE);
+        ExtractableResponse<Response> createSecondLayoutResponse = create_layout_request(UPDATE_NAME, Arrays.asList(secondProductId));
+        check_layout_created(createSecondLayoutResponse);
+        String thirdProductId = create_product(THIRD_PRODUCT_NAME, THIRD_PRODUCT_PRICE);
+        String id = get_id_from_response(createFirstLayoutResponse);
+
+        // when
+        ExtractableResponse<Response> updateLayoutResponse = update_layout_request(id, UPDATE_NAME, Arrays.asList(thirdProductId));
+        // then
+        check_layout_not_updated(updateLayoutResponse);
+    }
+
+
+    @Test
     void delete_layout() {
         // given
         String productId = create_product(FIRST_PRODUCT_NAME, FIRST_PRODUCT_PRICE);
@@ -93,12 +140,17 @@ public class LayoutAcceptanceTest extends AcceptanceTest {
         check_layout_deleted(deleteLayoutResponse);
     }
 
-    private ExtractableResponse<Response> create_layout_request(String name, List<String> productIds) {
+    private ExtractableResponse<Response> create_layout_request(String name,
+        List<String> productIds) {
         return create_request(new LayoutRequest(name, productIds), LAYOUT_API);
     }
 
     private void check_layout_created(ExtractableResponse<Response> response) {
         check_create_response(response);
+    }
+
+    private void check_layout_not_created(ExtractableResponse<Response> response) {
+        check_bad_request_response(response);
     }
 
     private ExtractableResponse<Response> find_layout_request(String id) {
@@ -108,6 +160,11 @@ public class LayoutAcceptanceTest extends AcceptanceTest {
     private void check_layout_found(ExtractableResponse<Response> response) {
         check_ok_response(response);
     }
+
+    private void check_layout_not_found(ExtractableResponse<Response> response) {
+        check_not_found_response(response);
+    }
+
 
     private ExtractableResponse<Response> find_layouts_request() {
         return find_request(LAYOUT_API);
@@ -124,6 +181,10 @@ public class LayoutAcceptanceTest extends AcceptanceTest {
 
     private void check_layout_updated(ExtractableResponse<Response> response) {
         check_ok_response(response);
+    }
+
+    private void check_layout_not_updated(ExtractableResponse<Response> response) {
+        check_bad_request_response(response);
     }
 
     private ExtractableResponse<Response> delete_layout_request(String id) {
